6ea432ae80c6aaaf032f9bdc2589e12b6640710b
ssuminii <tnalsdldkr2@naver.com>
2024-03-21 01:52:36 +0900

    second-commit

--- src/App.js
+++ src/App.js
@@ -27,18 +27,63 @@ const choice = {
 
 function App() {
   const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
 
   // userChoice -> rock, scissors, paper
   const play = (userChoice) => {
+    // 3. 버튼을 클릭하면 클릭한 아이템이 박스에 보임
     setUserSelect(choice[userChoice]);
+
+    // 4. 버튼을 클릭하면 컴퓨터 아이템은 랜덤으로 선택됨
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+
+    // 5. 3, 4번의 아이템들로 누가 이겼는지 승패를 나눔
+    setResult(judgement(choice[userChoice], computerChoice));
+  };
+
+  const judgement = (user, computer) => {
+    console.log('user', user, 'computer', computer);
+
+    // user == computer -> tie
+    // user == rock, computer == scissors -----> user win
+    // user == rock, computer == paper --------> user lose
+    // user == scissors, computer == paper ----> user win
+    // user == scissors, computer == rock -----> user lose
+    // user == paper, computer == rock --------> user win
+    // user == paper, computer == scissors ----> user lose
+    if (user.name == computer.name) {
+      return "Tie";
+    } else if (user.name == "Rock") {
+      return computer.name == "Scissors" ? "Win" : "Lose";
+    } else if (user.name == "Scissors") {
+      return computer.name == "Paper" ? "Win" : "Lose";
+    }else if (user.name == "Paper") {
+      return computer.name == "Rock" ? "Win" : "Lose";
+    }
+  }
+
+  // 초이스를 배열로 만들어 그 배열 인덱스값으로 랜덤값 만들기
+  const randomChoice = () => {
+    // 객체에 키 값만 뽑아서 array로 만들어주는 함수
+    let itemArray = Object.keys(choice);  // (3) ['rock', 'scissors', 'paper']
+    // console.log('itemArray------------', itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    // console.log('random Value-----------', randomItem);
+    let final = itemArray[randomItem];
+    // console.log('final----------',final);
+    return choice[final];
   };
 
   return (
     <div>
+      {/* 1. 박스 두개 만들기 (타이틀, 사진, 결과) */}
       <div className='main'>
-        <Box title='You' item={userSelect}/>
-        <Box title='computer' />
+        <Box title='You' item={userSelect} result={result}/>
+        <Box title='computer' item={computerSelect} result={result}/>
       </div>
+      {/* 2. 박스 하단에 가위바위보 버튼 */}
       <div className='main'>
         <button onClick={() => play("scissors")}>가위</button>
         <button onClick={() => play("rock")}>바위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 const Box = (props) => {
-  console.log("props", props)
+  // console.log("props", props);
+
+  let result;
+  if (props.title === "computer" && props.result !== "Tie" && props.result !== "") {
+    result =  (props.result === "Win" ? "Lose" : "Win");
+  } else {
+    result = props.result;
+  }
+
   return (
-    <div className='box'>
+    <div className={`box ${result}`}>
         <h1>{props.title}</h1>
         <img className='item-img' src={props.item && props.item.img} alt="" />
-        <h1>Winner</h1>  
+        <h1>{result}</h1>  
     </div>
   )
 }
